726683a06c0ee575d431f07e8d0c1807426aec61
Ahmed yehia <yehiaahmed900@gmail.com>
2023-05-17 00:39:03 +0200

    Add project files.

---
+++ Bank_System/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Bank_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Data Source=DESKTOP-FJPI0T1;Initial Catalog=BANK_APP;Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand command = connection.CreateCommand();
+            command.CommandText = "SELECT * FROM BANK";
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(reader["CODE"] + " " + reader["BANK_ADDRESS"] + " " + reader["BNAME"]);
+            }
+            Console.WriteLine("Connection Open ! ");
+            connection.Close();
+            Console.WriteLine("Connection closed ! ");
+        }
+    }
+}
