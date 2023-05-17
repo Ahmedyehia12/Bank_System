b7cb89c1448904ef492b01316efc2bd684111caa
mariaehab01 <maria.ehab2004@gmail.com>
2023-05-17 14:47:26 +0300

    update BANK table

--- Bank_System/Program.cs
+++ Bank_System/Program.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,19 +10,32 @@ namespace Bank_System
     {
         static void Main(string[] args)
         {
-            string connectionString = "Data Source=DESKTOP-FJPI0T1;Initial Catalog=BANK_APP;Integrated Security=True";
+            System.Console.WriteLine("Hello World!");
+
+            var datasource = @"localhost";//your server
+            var database = "bank_system"; //your database name
+            var username = "SA"; //username of server to connect
+            var password = "igpvmk123"; //password
+
+            //your connection string 
+            string connectionString = @"Data Source=" + datasource + ";Initial Catalog="
+                        + database + ";Persist Security Info=True;User ID=" + username + ";Password=" + password;
+
             SqlConnection connection = new SqlConnection(connectionString);
             connection.Open();
+            Console.WriteLine("ayhaga wenaby");
             SqlCommand command = connection.CreateCommand();
             command.CommandText = "SELECT * FROM BANK";
             SqlDataReader reader = command.ExecuteReader();
             while (reader.Read())
             {
-                Console.WriteLine(reader["CODE"] + " " + reader["BANK_ADDRESS"] + " " + reader["BNAME"]);
+                Console.WriteLine(reader["BANK_CODE"] + " " + reader["BANK_ADDRESS"] + " " + reader["BANK_NAME"]);
             }
             Console.WriteLine("Connection Open ! ");
             connection.Close();
             Console.WriteLine("Connection closed ! ");
+            Console.WriteLine("helkkkooooo");
+            Console.ReadLine();
         }
     }
 }
