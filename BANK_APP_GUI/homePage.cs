5b5287ecc405411053a542f3ad3aaa88e7c65011
Merna Islam <101946404+mernaislam@users.noreply.github.com>
2023-05-19 22:57:58 +0200

    home screen done

---
+++ BANK_APP_GUI/homePage.cs
@@ -0,0 +1,41 @@
+namespace BANK_APP_GUI
+{
+    public partial class homePage : Form
+    {
+        public homePage()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // sign up
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// sign in
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
